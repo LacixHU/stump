c1521ebf51c298dcade741575f3599c61f5edabb
László Hatvani <lhatvani@cadproject.hu>
2026-05-13 22:24:41 +0200

    comics settings dialog x close button

--- packages/browser/src/components/readers/imageBased/container/SettingsDialog.tsx
+++ packages/browser/src/components/readers/imageBased/container/SettingsDialog.tsx
@@ -20,6 +20,10 @@ export default function SettingsDialog() {
 			</Dialog.Trigger>
 
 			<Dialog.Content size="md" className="gap-4 z-101 flex flex-col bg-background-surface">
+				<Dialog.Header className="flex items-center justify-end">
+					<Dialog.Close />
+				</Dialog.Header>
+
 				<Tabs
 					value={modality}
 					defaultValue="book"
